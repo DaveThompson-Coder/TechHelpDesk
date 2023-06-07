17d62949579066e9257b511336354b81aa752680
Dave Thompson <davethompson59@aol.com>
2023-06-08 00:50:09 +0100

    Data Models added for Ticket Lookup Tables

---
+++ Models/TicketType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechHelpDesk.Models
+{
+    public class TicketType
+    {
+        public int Id { get; set; }         //Primary key
+                                            //This Table Creates a 1 to 1 Relationship
+        [DisplayName("Type Name")]          //Between the Ticket & the Ticket Type
+        public string Name { get; set; }    //Because Each Ticket can Only be of One Type
+    }
+}       
