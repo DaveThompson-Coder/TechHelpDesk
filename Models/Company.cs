45b6ec6901110142a21598b78d82389fa4e115b0
Dave Thompson <davethompson59@aol.com>
2023-06-11 00:48:58 +0100

    Added Company Data Model

---
+++ Models/Company.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechHelpDesk.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }     //Primary key
+
+        [DisplayName("Company Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Company Description")]
+        public string Description { get; set; }
+
+        // --Navigation Properties -- //
+        public virtual ICollection<HDUser> Members { get; set; }
+        public virtual ICollection<Project> Projects { get; set; }
+
+    }
+}
