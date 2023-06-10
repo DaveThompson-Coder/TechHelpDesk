f9204d2c0de6989bf36631848f08e2505702a6ea
Dave Thompson <davethompson59@aol.com>
2023-06-11 00:34:45 +0100

    Added Project & ProjectPrority Data Models

---
+++ Models/Project.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace TechHelpDesk.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }     //Primary key
+
+        [DisplayName("Company")]
+        public int? CompanyId { get; set; }     //nullable Foreign key to the Company Table
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Project Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [DisplayName("Start Date")]
+        [DataType(DataType.Date)]
+        public DateTimeOffset StartDate { get; set; }
+
+        [DisplayName("End Date")]
+        [DataType(DataType.Date)]
+        public DateTimeOffset EndDate { get; set; }
+
+        [DisplayName("Priority")]
+        public int? ProjectPriorityId { get; set; }     //nullable Foreign key to the Project Priority Table
+
+        //Attachment information fields
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile ImageFormFile { get; set; }
+
+        [DisplayName("File Name")]
+        public string ImageFileName { get; set; }
+
+        public byte[] ImageFileData { get; set; }
+
+        [DisplayName("File Extension")]
+        public string ImageContentType { get; set; }
+
+        [DisplayName("Archived")]
+        public bool Archived { get; set; }
+
+        // -- Navigation properties -- //
+        public virtual Company Company { get; set; }
+        public virtual ProjectPriority ProjectPriority { get; set; }
+
+        public virtual ICollection<HDUser> Members { get; set; } = new HashSet<HDUser>();            //Sets up one to many relationships
+        public virtual ICollection<Ticket> Tickets { get; set; } = new HashSet<Ticket>();
+
+    }
+}
