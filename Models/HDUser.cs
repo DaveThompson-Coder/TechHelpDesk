efc361430d6d2abd107ec028ae079c7c5c5d0814
Dave Thompson <davethompson59@aol.com>
2023-06-11 01:05:19 +0100

    Edited HDUser.cs file

--- Models/HDUser.cs
+++ Models/HDUser.cs
@@ -27,6 +27,23 @@ namespace TechHelpDesk.Models
         [Display(Name = "Full Name")]
         public string FullName { get { return $"{FirstName} {LastName}"; } }
 
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile AvatarFormFile { get; set; }
+
+        [DisplayName("Avatar")]
+        public string AvatarFileName { get; set; }
+        public byte[] AvatarFileData { get; set; }
+
+        [Display(Name = "File Extension")]
+        public string AvatarContentType { get; set; }
+
+
+        public int? CompanyId { get; set; }
+
+        // -- Navigation properties -- //
+        public virtual Company Company { get; set; }
+        public virtual ICollection<Project> Projects { get; set; }
 
 
     }
