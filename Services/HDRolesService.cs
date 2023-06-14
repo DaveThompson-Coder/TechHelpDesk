410caaf525d1ed9d799f128640d9eb6213078dca
Dave Thompson <davethompson59@aol.com>
2023-06-14 17:06:06 +0100

    Added ‘HDRolesService’ implementation class & Dependency injection

---
+++ Services/HDRolesService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechHelpDesk.Models;
+using TechHelpDesk.Services.Interfaces;
+
+namespace TechHelpDesk.Services
+{
+    public class HDRolesService : IHDRolesService
+    {
+        public Task<bool> AddUserToRoleAsync(HDUser user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetRoleNameByIdAsync(string roleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<string>> GetUserRolesAsync(HDUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<HDUser>> GetUsersInRoleAsync(string roleName, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<HDUser>> GetUsersNotInRoleAsync(string roleName, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUserInRoleAsync(HDUser user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserFromRoleAsync(HDUser user, IEnumerable<string> roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserFromRolesAsync(HDUser user, IEnumerable<string> roles)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/Interfaces/IHDRolesService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechHelpDesk.Models;
+
+namespace TechHelpDesk.Services.Interfaces
+{
+    public interface IHDRolesService
+    {
+        public Task<bool> IsUserInRoleAsync(HDUser user, string roleName);
+
+        public Task<IEnumerable<string>> GetUserRolesAsync(HDUser user);        //Returns a list of strings if you have more than one role
+
+        public Task<bool> AddUserToRoleAsync(HDUser user, string roleName);     //Add user to Roles List  
+
+        public Task<bool> RemoveUserFromRoleAsync(HDUser user, IEnumerable<string> roleName);   //Remove user from a single Role
+
+        public Task<bool> RemoveUserFromRolesAsync(HDUser user, IEnumerable<string> roles);     //Remove user from multiple Roles
+
+
+        public Task<List<HDUser>> GetUsersInRoleAsync(string roleName, int companyId);     //Get Users in Role for a specific Company
+
+        public Task<List<HDUser>> GetUsersNotInRoleAsync(string roleName, int companyId);   //Get Users Not in Role for a specific Company
+
+        public Task<string> GetRoleNameByIdAsync(string roleId);    //Get Role Name based on it's Id
+    }
+}
