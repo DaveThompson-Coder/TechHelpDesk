1eb667ff9753f5e7777270b3a2828e680970be88
Dave Thompson <davethompson59@aol.com>
2023-06-16 23:30:30 +0100

    Added IHDCompanyInfoService interface

---
+++ Services/Interfaces/IHDCompanyInfoService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechHelpDesk.Models;
+
+namespace TechHelpDesk.Services.Interfaces
+{
+    public interface IHDCompanyInfoService
+    {
+        public Task<Company> GetCompanyInfoByIdAsync(int? companyId);
+
+        public Task<List<HDUser>> GetAllMembersAsync(int companyId);
+
+        public Task<List<Project>> GetAllProjectsAsync(int companyId);
+
+        public Task<List<Ticket>> GetAllTicketAsync(int ticketId);
+
+    }
+}
