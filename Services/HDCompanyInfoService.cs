be440f1323e108f7b999a84d7edd805736c1ad8d
Dave Thompson <davethompson59@aol.com>
2023-06-16 23:54:21 +0100

    Add HDCompanyInfoService link to IHDCompanyInfoService inject ApplicationDbContext to access datbase & add implementation code to GetAllMembersAsync Method

---
+++ Services/HDCompanyInfoService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechHelpDesk.Data;
+using TechHelpDesk.Models;
+using TechHelpDesk.Services.Interfaces;
+
+namespace TechHelpDesk.Services
+{
+    public class HDCompanyInfoService : IHDCompanyInfoService
+    {
+        private readonly ApplicationDbContext _context;
+        
+        public HDCompanyInfoService(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+
+        public async Task<List<HDUser>> GetAllMembersAsync(int companyId)
+        {
+            List<HDUser> result = new();
+
+            result = await _context.Users.Where(u => u.CompanyId == companyId).ToListAsync();
+
+            return result;
+        }
+
+        public Task<List<Project>> GetAllProjectsAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketAsync(int ticketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Company> GetCompanyInfoByIdAsync(int? companyId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
