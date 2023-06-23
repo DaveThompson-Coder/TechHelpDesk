d4813284e2a9ea07da18c3119338a91ff6e5317c
Dave Thompson <davethompson59@aol.com>
2023-06-23 17:11:00 +0100

    The Ticket CRUD Methods, Added IHDTicketService to interfaces folder in Services & added Method definitions, Added HDTicketService in Services folder, & added implentation classes to the Service, injected ‘ApplicationDBContext’ into the HDTicketService, & Added code to AddNewTicket, UpdateTicketAsync, GetTicketByIdAsync, ArchiveTicketAsync Methods

---
+++ Services/HDTicketService.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechHelpDesk.Data;
+using TechHelpDesk.Models;
+using TechHelpDesk.Services.Interfaces;
+
+namespace TechHelpDesk.Services
+{
+    public class HDTicketService : IHDTicketService
+    {
+        private readonly ApplicationDbContext _context;
+
+        //injection of Services into This Service
+        public HDTicketService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        //CRUD Operation - Create New Ticket Method
+        public async Task AddNewTicketAsync(Ticket ticket)
+        {
+            try
+            {
+                _context.Add(ticket);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        //CRUD Operation - Delete/Archive Ticket Method
+        public async Task ArchiveTicketAsync(Ticket ticket)
+        {
+            ticket.Archived = true;
+            _context.Update(ticket);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task AssignTicketAsync(int ticketId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketsByCompanyAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketsByPriorityAsync(int companyId, string priorityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketsByStatusAsync(int companyId, string statusName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketsByTypeAsync(int companyId, string typeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetArchivedTicketsAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetProjectTicketsByPriorityAsync(string priorityName, int companyId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetProjectTicketsByRoleAsync(string role, string userId, int projectId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetProjectTicketsByStatusAsync(string statusName, int companyId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetProjectTicketsByTypeAsync(string typeName, int companyId, int projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        //CRUD Operation - Read Ticket Method
+        public async Task<Ticket> GetTicketByIdAsync(int ticketId)
+        {
+            try
+            {
+                return await _context.Tickets.FirstOrDefaultAsync(t => t.Id == ticketId);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public Task<HDUser> GetTicketDeveloperAsync(int ticketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetTicketsByRoleAsync(string role, string userId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetTicketsByUserIdAsync(string userId, int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int?> LookupTicketPriorityIdAsync(string priorityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int?> LookupTicketStatusIdAsync(string statusName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int?> LookupTicketTypeIdAsync(string typeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        //CRUD Operation - Update Ticket Method
+        public async Task UpdateTicketAsync(Ticket ticket)
+        {
+            try
+            {
+                _context.Update(ticket);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
